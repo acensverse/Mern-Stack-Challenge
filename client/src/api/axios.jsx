b5027a45845c437178b14dc09614c024c8eea10e
acensverse <acensverse@gmail.com>
2024-10-25 15:50:12 +0530

    deploy

--- client/src/api/axios.jsx
+++ client/src/api/axios.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://mern-stack-challenge-r8w2.vercel.app/'
+    baseURL: 'https://mern-stack-challenge-server.vercel.app/'
 });
